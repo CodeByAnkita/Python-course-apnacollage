9de021c3cf6536631fa3693be4181a813cadbb43
CodeByAnkita <ankitacode11@gmail.com>
2024-10-18 11:20:35 +0530

    added some sample code

--- FirstProgram.py
+++ FirstProgram.py
@@ -7,3 +7,15 @@ name3='''pn'''
 print (name1)
 print (name2)
 print (name3) 
+
+light = input("lighy=t color: ")
+
+if(light == "red"):
+    print("Turn on the red light")
+elif(light == "green"):
+    print("Turn on the green light")
+elif(light == "yellow"):
+    print("Turn on the yellow light")
+else:
+    print("Invalid color input")
+    
